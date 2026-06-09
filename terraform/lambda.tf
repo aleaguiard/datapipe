33049fdf3728bd7192bfd17c4932df00c07f48f0
Ale Aguiar Díaz <aleaguiard@gmail.com>
2026-06-09 10:58:12 +0100

    fix: enable ReportBatchItemFailures on SQS ESM, fix processor-errors alarm to use DLQ metric

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -121,8 +121,9 @@ resource "aws_lambda_function" "rows" {
 }
 
 resource "aws_lambda_event_source_mapping" "processor_sqs" {
-  event_source_arn = aws_sqs_queue.processing.arn
-  function_name    = aws_lambda_function.processor.arn
-  batch_size       = 1
-  enabled          = true
+  event_source_arn                   = aws_sqs_queue.processing.arn
+  function_name                      = aws_lambda_function.processor.arn
+  batch_size                         = 1
+  enabled                            = true
+  function_response_types            = ["ReportBatchItemFailures"]
 }
